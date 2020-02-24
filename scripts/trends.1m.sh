dd75efbbec7bcdd57b4f7602fc667d0ec44e53dc
ihsan <ihsanl@pm.me>
2020-02-24 15:39:28 +0300

    show only one line result from eksi

--- scripts/trends.1m.sh
+++ scripts/trends.1m.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "`/usr/local/bin/trends -erl2|tr '[:upper:]' '[:lower:]'|sed 's/$/ | color=#90BD58/g'`"
-echo "`/usr/local/bin/trends -l2 -ntr|tr '[:upper:]' '[:lower:]'|sed 's/$/ | color=yellow/g'`"
+echo "`/usr/local/bin/trends -erl1|tr '[:upper:]' '[:lower:]'|sed 's/$/ | color=#90BD58/g'`"
+# echo "`/usr/local/bin/trends -l1 -ntr|tr '[:upper:]' '[:lower:]'|sed 's/$/ | color=yellow/g'`"
